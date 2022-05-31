8d408ee90ae534a317f058d51293462c6d0caab5
Sidharth <70311553+C0NN3C73D@users.noreply.github.com>
2022-05-31 14:17:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,14 +15,14 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'ded8e37268c7', //credit goes to sidhut, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
 global.owner = ['Your Number']
 global.premium = ['Your Number']
 global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.ownername = '🐦sidhu'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
